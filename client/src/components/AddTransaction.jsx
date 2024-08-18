d39a22a7e14386256127f81cfbcc53c9d3a2b5ed
Moyo-Sa <128077572+Moyo-Sa@users.noreply.github.com>
2024-08-18 11:09:58 -0400

    Add files via upload

---
+++ client/src/components/AddTransaction.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
+export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (text.trim() === "" || amount === 0) {
+      alert("Please enter a valid text and amount");
+      return;
+    }
+
+    const newTransaction = {
+      //id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Expense</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (put '-' for expense, and '+' for income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
---
+++ client/src/components/TransactionList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
+import { useContext, useEffect } from "react";
+
+export const TransactionList = () => {
+  const { transactions, getTransactions } = useContext(GlobalContext);
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction._id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+};
