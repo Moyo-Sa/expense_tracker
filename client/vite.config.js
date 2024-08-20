29c727ca11262dd86c13b7a40457bbc64f838161
Moyo-Sa <128077572+Moyo-Sa@users.noreply.github.com>
2024-08-19 21:03:56 -0400

    Update with correct backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_TARGET,
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
